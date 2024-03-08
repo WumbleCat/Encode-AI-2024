46eb7ab36ed1899e147de8e0fe1b48048079ee5f
WumbleCat <komethtauch04@gmail.com>
2024-03-08 22:39:11 +0000

    transaction between two parties

--- generate_qr_png.py
+++ generate_qr_png.py
@@ -1,6 +1,8 @@
 import os
 from solathon.solana_pay import create_qr
 
+url = "solana:mvines9iiHiQTysrwkJjGf2gb9Ex9jXJX8ns3qwf2kN?amount=1e-09&label=Jungle+Cats+store&message=Jungle+Cats+store+-+your+order+-+%23001234&memo=JC%234098&reference=6DgHPm8gQp2mvQs5wuYTbJCH5KkkdJ7i4P76M31aegCQ"
+
 def generate_qr_img(url):
     # Generate the QR Code
     qr_image = create_qr(url)
