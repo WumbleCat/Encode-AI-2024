5e6bda194ee78ddc45eb8433b7d81032155f5fcf
WumbleCat <komethtauch04@gmail.com>
2024-03-09 00:16:36 +0000

    get balance

--- create_new_acc.py
+++ create_new_acc.py
@@ -1,6 +1,14 @@
-from solathon import Client, Keypair
+from solathon import Client, Keypair, PublicKey
 
+# Client always stays the same, since we're using solana
 client = Client("https://api.devnet.solana.com")
 
+# Create public and private keys
 new_account = Keypair()
 print(new_account.public_key, new_account.private_key)
+
+# Get balance 
+public_key = PublicKey("B3BhJ1nvPvEhx3hq3nfK8hx4WYcKZdbhavSobZEA44ai")
+balance = client.get_balance(public_key)
+
+print(balance)
